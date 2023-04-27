a7a7247360ed512e8eee930530f5e48bb7cb9453
DMFeijo <danielmourafeijo@gmail.com>
2023-04-27 11:12:13 -0300

    commit 1

---
+++ cypress/e2e/barriga_registro.cy.js
@@ -0,0 +1,50 @@
+describe('Testes de registro', () => {
+  beforeEach(() => {
+    cy.visit('https://barrigareact.wcaquino.me/')
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.url().should('include', '/registro')
+  });
+  it('Teste de registro bem-sucedido', () => {
+
+    //Interagir com o campo "nome"
+    cy.get('[placeholder=Nome]').type('Daniel Feijo')
+    //Interagir com o campo "email"
+    cy.get('[placeholder=Email]').type('daniel_cy@teste.com')
+    //Interagir com o campo "senha"
+    cy.get('[placeholder=Senha]').type('senha123')
+    //Clicar no botão "registrar"
+    cy.get('.btn').click()
+    //Interagir com o campo "nome"
+    cy.contains('Usuário adicionado com sucesso')
+  })
+  it('Teste de registro sem o preenchimento do campo "nome"', () => {
+
+    cy.get('[placeholder=Email]').type('teste_cy@teste.com')
+    cy.get('[placeholder=Senha]').type('senha123')
+    cy.get('.btn').click()
+    cy.contains('Erro')
+    
+  });
+  it('Teste de registro sem o preenchimento do campo "email"', () => {
+
+    cy.get('[placeholder=Nome]').type('teste_cy@teste.com')
+    cy.get('[placeholder=Senha]').type('senha123')
+    cy.get('.btn').click()
+    cy.contains('Erro')
+    
+  });
+  it('Teste de registro sem o preenchimento do campo "senha"', () => {
+    
+    cy.get('[placeholder=Nome]').type('teste_cy@teste.com')
+    cy.get('[placeholder=Email]').type('senha123')
+    cy.get('.btn').click()
+    cy.contains('Erro')
+
+  });
+  it.only('Teste de registro sem o preenchimento dos campos', () => {
+    
+    cy.get('.btn').click()
+    cy.contains('status code 500')
+
+  });
+})
